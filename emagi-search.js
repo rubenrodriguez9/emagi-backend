ad6150ad42f7ff2fab0da650061197149d68187f
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-07-09 12:07:18 -0400

    Finished, but late and needed help from Mesi

--- emagi-search.js
+++ emagi-search.js
@@ -3,6 +3,12 @@ const symbols = require(('./emagi-symbols.js'))
 
 const emagiSearch = function(str){
     result = []
+    for(const statement of words){
+        console.log(statement)
+        if(statement.includes(str)){
+            result.push(symbols[words.indexOf(statement)])
+        }
+    }
     
     
     return result
